20a1f178b16689109d359e744dbef07ca6229dcb
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-09-21 10:07:15 -0400

    removed :first_name from :users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200914210336) do
+ActiveRecord::Schema.define(version: 20200921140445) do
 
   create_table "stories", force: :cascade do |t|
     t.string  "title"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20200914210336) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "first_name"
     t.string "password_digest"
   end
 
